0aee4092be6c4f81dc348445373d19251a1b5952
dponinski <david@poninski.com>
2013-03-09 13:04:24 -0600

    Started coding out theme options and customization options. Adding job post type and working on all the meta boxes for it.

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ require_once('library/bones.php'); // if you remove this, bones will break
     - example custom taxonomy (like categories)
     - example custom taxonomy (like tags)
 */
-require_once('library/custom-post-type.php'); // you can disable this if you like
+require_once('library/job-post-type.php'); // you can disable this if you like
 /*
 3. library/admin.php
     - removing some default WordPress dashboard widgets
@@ -43,7 +43,13 @@ require_once('library/admin.php'); // this comes turned off by default
 4. library/translation/translation.php
     - adding support for other languages
 */
-// require_once('library/translation/translation.php'); // this comes turned off by default
+require_once('library/translation/translation.php'); // this comes turned off by default
+/*
+5. library/theme-options.php
+    - adding custom theme options
+*/
+require_once('library/theme-options.php'); // this comes turned off by default
+
 
 /************* THUMBNAIL SIZE OPTIONS *************/
 
